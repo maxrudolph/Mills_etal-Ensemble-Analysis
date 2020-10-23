aa6b1d4cbdea87a8ae0c6edc88097f4fc99e7f12
cmills1095 <56559868+cmills1095@users.noreply.github.com>
2020-10-23 14:11:51 -0700

    Update ensembleAnalysis.m

--- ensembleAnalysis.m
+++ ensembleAnalysis.m
@@ -1,4 +1,6 @@
-function ensembleAnalysis(results,data,model)
+function ensembleAnalysis(filename)
+load(filename)
+
 ifDebugging = true;
 
 if ifDebugging
@@ -73,7 +75,7 @@ end
     plotLambda = makeLambda(xVals);
     
     for i=1:nSavedPlot
-        yVals(:,i) = model(squeeze(results.storedDepths(:,ind(i))),...
+        yVals(:,i) = forwardModel(squeeze(results.storedDepths(:,ind(i))),...
             results.storedRhos(:,ind(i)),plotLambda);
     end
     
@@ -93,68 +95,87 @@ end
     hdata = loglog(data.x,data.y,'r.','MarkerSize',10.0);
     hexact = loglog(data.x,data.fx,'r-','LineWidth',1.5);
     
-    %{
-    % find solution with median misfit from ensemble
-    [meanModel, medianModel, bestFitModel] = createMeanModel(...
-        length(results.storedRhos(:,1)),measure,results);
+    %Mean model
+    minDist = log10(data.x(1));
+    maxDist = log10(data.x(end));
     
+    numLayersMean = results.maxLayers(end)*10;
+    meanModelDepths = [0,logspace(0,maxDist, numLayersMean-1)];
+    meanModelRhos = zeros(numLayersMean,1);
+    for i = 1:size(meanModelDepths,2) %for each imaginary layer...
+        for j = 1:size(results.storedDepths,2) % look at each run...
+            indx = nnz(meanModelDepths(i)>=results.storedDepths(:,j)); 
+            %find the actual layer this imaginary layer lies in
+            meanModelRhos(i) = meanModelRhos(i) + ...
+                log10(results.storedRhos(indx,j));
+            % add the corresponding resistivity to the total
+        end
+        meanModelRhos(i) = 10.^(meanModelRhos(i)/...
+            size(results.storedDepths,2)); %find average
+    end
    
-    hMedian = plot(data.x,model(medianModel.depths,medianModel.rhos,...
-        data.lambda),'LineWidth',2);
-    hBest = plot(data.x,model(bestFitModel.depths,bestFitModel.rhos,...
-        data.lambda),'LineWidth',2);
-
-hmean=loglog(x_vals,mean(y_vals,2),'k','LineWidth',1); %Ensemble mean
-
-
-hEnsembleMean = loglog(newX,calculateRho1D(meanDepths,meanRhos,meanLambda),'g','LineWidth',2);
-%calculated mean
-set(gca,'FontSize',12);
-set(gca,'Color','w');
-set(gca,'XScale','log');
-set(gca,'YScale','log');
-
-xlabel('Array spacing (m)');
-ylabel('Apparent Resistivity (\Omega-m)')
+    meanModelX = logspace(minDist,maxDist,numLayersMean); 
+    %new imaginary measurements
+    meanModelLambda = makeLambda(meanModelX);
     
-hmean=loglog(xVals,mean(yVals,2),'k','LineWidth',1); %Ensemble mean
-hOtherMean = loglog(newX,calculateRho1D(meanDepths,meanRhos,meanLambda),'g*');
-%calculated mean
-set(gca,'FontSize',16);
-set(gca,'Color','w');
-set(gca,'XScale','log');
-set(gca,'YScale','log');
-legend([hexact,hdata,h1,hmean,hOtherMean],...
-    {'Exact','Data+noise','Ensemble','Ensemble mean','Calculated mean'},...
-    'Location','southwest');
+    [~,ind] = sort(results.ensembleMisfits);
+    medianIndex= ind(floor(length(ind)/2));
+    bestIndex = ind(1);
+    medianModelDepths = results.storedDepths(:,medianIndex);
+    bestFitModelDepths = results.storedDepths(:,bestIndex);
+    medianModelRhos = results.storedRhos(:,medianIndex);
+    bestFitModelRhos = results.storedRhos(:,bestIndex);
+    
+    hMean = plot(meanModelX,forwardModel(meanModelDepths,meanModelRhos,...
+        meanModelLambda),'g-','LineWidth',2);
+    hMedian = plot(data.x,forwardModel(medianModelDepths,medianModelRhos,...
+        data.lambda),'LineWidth',2);
+    hBest = plot(data.x,forwardModel(bestFitModelDepths,...
+        bestFitModelRhos,data.lambda),'LineWidth',2);
+    
+    hEnsembleMean = loglog(xVals,mean(yVals,2),'k','LineWidth',1);
+    
+    set(gca,'FontSize',12);
+    set(gca,'Color','w');
+    set(gca,'XScale','log');
+    set(gca,'YScale','log');
 
-%% Plot the model
-depth_plot = zeros(length(xVals),nSaved);
-rho_plot = zeros(size(depth_plot));
-for i=1:nSaved
-    % interpolate resistivity onto x-grid
-    depth_plot(:,i) = log10(xVals);
-    nlayer = length(~isnan(results.storedDepths(:,i)));
-    for j=1:nlayer
+    xlabel('Array spacing (m)');
+    ylabel('Apparent Resistivity (\Omega-m)')
+    
+legend([hexact,hdata,hMean,hMedian,hBest,hEnsembleMean,hEnsemble],...
+    {'Exact','Data+noise','Mean model','Median model','Best fit model',...
+    'Ensemble mean','Ensemble'},'Location','southwest');
+
+%% Color plot
+depthPlot = zeros(length(xVals),nSaved);
+rhoPlot = zeros(size(depthPlot));
+for i = 1:nSaved
+    %interpolate resistivity onto x-grid
+    depthPlot(:,i) = log10(xVals);
+    nLayer = length(~isnan(results.storedDepths(:,i)));
+    for j = 1:nLayer
         mask = log10(xVals) >= log10(results.storedDepths(j,i));
-        rho_plot(mask,i) = log10(results.storedRhos(j,i));
+        rhoPlot(mask,i) = log10(results.storedRhos(j,i));
     end
 end
+
 figure();
-hist3([rho_plot(:),depth_plot(:)],[100 100],'CDataMode','auto');
+hist3([rhoPlot(:),depthPlot(:)],[100 100],'CDataMode','auto');
 view(2);
 hold on
-true_rho_plot = zeros(size(xVals));
-true_depths_plot = depth_plot(:,1);
+trueRhoPlot = zeros(size(xVals));
+trueDepthsPlot = depthPlot(:,1);
+[trueDepths,trueRhos] = modelGen(results.maxLayers(end),thisMeasure.modelChoice);
+trueNumLayers = nnz(~isnan(trueDepths));
 for j=1:trueNumLayers
     mask = log10(xVals) >= log10(trueDepths(j));
-    true_rho_plot(mask) = log10(trueRhos(j));   
+    trueRhoPlot(mask) = log10(trueRhos(j));   
 end
-plot(true_rho_plot,true_depths_plot,'r');
+plot(trueRhoPlot,trueDepthsPlot,'r-','LineWidth',30);
 colorbar();
 set(gca,'YDir','reverse');
 xlabel('log10(Resistivity)');
 ylabel('log10(Depth)');
-plot(log10(meanRhos),log10(meanDepths),'g')
-    %}
+plot(log10(meanModelRhos),log10(meanModelDepths),'g-','LineWidth',30)
 end
