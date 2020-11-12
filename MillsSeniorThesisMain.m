15d15e7379c9c2b13f2aae1a59003612e6132e9d
Max Rudolph <maxrudolph@ucdavis.edu>
2020-11-12 09:53:25 -0800

    restore parfor loop

--- MillsSeniorThesisMain.m
+++ MillsSeniorThesisMain.m
@@ -19,18 +19,18 @@ if ifLoadData
     % ...Not setup yet to take real data...
 else
     %Artifical model setup:
-    measure.modelChoice = '4LayerA'; %currently setup: 3LayerA, 4LayerA
+    measure.modelChoice = '3LayerA'; %currently setup: 3LayerA, 4LayerA
     %Measurement options
     measure.minDist = 0.1; % Smallest electrode distance, meters
     measure.maxDist = 1000; %  Largest electrode distance, meters
     measure.numMeasurements = 21; %total # of measurements
-    measure.noiseCoef = 0.1; %How "noisy" are the measurements
+    %measure.noiseCoef = 0.01; %How "noisy" are the measurements
 end
 
 %% Set inversion options
 options.kMax = 10; %max number of layers allowed in models
 options.numSteps = 5e7; %total iterations for MCMC loop. 1e7+ recommended
-options.mLPSCoefficient = 1e5;
+options.mLPSCoefficient = 1e4;
 options.modelChoice = measure.modelChoice;
 %mLPS = max layers per step. Set higher for longer 'burn-in' period.
 options.saveStart = floor(options.numSteps/2);
@@ -55,17 +55,16 @@ if options.numSteps - options.saveStart < options.mLPSCoefficient*...
 end
 
 measure.kMax = options.kMax;
-data = createSyntheticData(measure, forwardModel); %creates measurements
 
 % Create bounds on parameter values. These bounds are based on Appendix A
 % in Malinverno 2002. See also the "genericMedium" constructor function
 %Bound parameters. Bounds based on Appendix A, Malinverno 2002
 pBounds.maxLayers = options.kMax; % max # of layers in a given model
-pBounds.depthMin = min(data.x); %min depth for layer interface (not top)
-pBounds.depthMax = max(data.x); % max depth for layer interface
+pBounds.depthMin = 1e-1; %min depth for layer interface (not top)
+pBounds.depthMax = 1e4; % max depth for layer interface
 pBounds.rhoMin = 1e-8; % min resistivity, NEEDS UPDATE
 pBounds.rhoMax = 1e8; % max resistivity, NEEDS UPDATE
-pBounds.varMin = 1e-10; % valid?  
+pBounds.varMin = 1e-10; % valid?
 pBounds.varMax = 1e10; % valid?
 pBounds.varChange = 1e-1;  %valid?
 pBounds.intlVar = options.intlVar; %initial variance
@@ -73,21 +72,22 @@ pBounds.numSteps = options.numSteps; %
 %% Do inversion
 %results = mcmcAlgorithm(data,forwardModel,options); %Do the inversion
 
-data = createSyntheticData(measure, forwardModel); %creates measurements
-results = mcmcAlgorithm(data,forwardModel,options, pBounds);
-%filename = ['Ensemble_', thisMeasure.modelChoice, '_',...
-%    num2str(thisMeasure.noiseCoef), '_', date, '.mat'];
-%doSaving(filename,results,data,thisMeasure,options,forwardModel);
+noiseCoefs=[0.0,0.01,0.02,0.05,0.1,0.2]
+parfor inoise=1:6
+	thisMeasure = measure;
+	thisMeasure.noiseCoef=noiseCoefs(inoise);
+	data = createSyntheticData(thisMeasure, forwardModel); %creates measurements
+	results = mcmcAlgorithm(data,forwardModel,options, pBounds);
+	%filename = ['Ensemble_', thisMeasure.modelChoice, '_',...
+	%    num2str(thisMeasure.noiseCoef), '_', date, '.mat'];
+	%doSaving(filename,results,data,thisMeasure,options,forwardModel);
 
-
-%% Plot Run Properties
-disp(['Plotting']);
-
-ifSave = true;%false;%input('save? true/false\n');
-if ifSave
-    filename = ['Ensemble_', measure.modelChoice, '_',...
-        num2str(measure.noiseCoef),'.mat'];
-    save(filename,'results','data','options','measure','forwardModel');
-%    ensembleAnalysis(filename);
+	ifSave = true;
+	if ifSave
+	    filename = ['Ensemble_', measure.modelChoice, '_',...
+        	num2str(measure.noiseCoef),'.mat'];
+	    save(filename,'results','data','options','thisMeasure','forwardModel');
+	    %    ensembleAnalysis(filename);
+	end
 end
-%}
+disp(['Plotting']);
