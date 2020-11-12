8be21ff9101712514f423b3d35c5731c68df7949
Max Rudolph <maxrudolph@ucdavis.edu>
2020-11-12 11:59:49 -0800

    add v7.3 flag to save - allow large files

--- doSaving.m
+++ doSaving.m
@@ -1,3 +1,3 @@
 function doSaving(filename,results,data,measure,options,forwardModel,pBounds)
-     save(filename,'results','data','options','measure','forwardModel','pBounds');
+     save(filename,'results','data','options','measure','forwardModel','pBounds','-v7.3');
 end
