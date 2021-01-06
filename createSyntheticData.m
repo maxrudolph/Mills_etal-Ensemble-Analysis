80f7821c01a21b5ff5d0309a2198f504a5948acb
cmills1095 <56559868+cmills1095@users.noreply.github.com>
2021-01-05 21:25:18 -0800

    Updating comments
    More comment updating, no actual changes

--- createSyntheticData.m
+++ createSyntheticData.m
@@ -1,16 +1,16 @@
 function data = createSyntheticData(measure, forwardModel)
 %{
     Creates artificial model and synthetic measurements for
-MillsSeniorThesisMain. The input structure, 'measure,' contains:
-minDist, maxDist: scalars, min and max electrode spacing during 
-measurements, in meters; numMeasurements: number of measurements taken;
-noiseCoef: How noisy the measurements are; modelChoice: which model to use;
-kMax: max number of layers (for inversion purposes). forwardModel is a
-function handle for the choice of forward model being used (script)
-    Creates: data a structure with fields x: values of electrode spacings
+MillsSeniorThesisMain and other fxns. The input structure, 'measure,'contains:
+minDist, maxDist: scalars, min and max electrode spacing in meters during 
+measurements; numMeasurements: number of measurements taken; noiseCoef: How 
+noisy the measurements are; modelChoice: which model to use; kMax: max 
+number of layers (for inversion purposes). forwardModel is a function 
+handle for the choice of forward model being used (script)
+    Creates: data, a structure with fields x: values of electrode spacings
 measurements; lambda: a 2D array based on x; fx: the model output with no
-noise; y: model output with noise.
-    Requires other scripts: modelGen, and something for forwardModel
+noise; y: model output with noise; Cd: covaraince matrix
+    forwardModel is a fxn handle used to create the data
     %}
     [trueDepths,trueRhos] = modelGen(measure.kMax,measure.modelChoice);
     minDist = log10(measure.minDist);
@@ -22,7 +22,7 @@ noise; y: model output with noise.
     data.fx = forwardModel(trueDepths,trueRhos,data.lambda); 
     %The "true" model output given above parameters
     noiseVector = measure.noiseCoef.*data.fx.*randn(length(data.fx),1);
-    %The absolute value of this shouldn't matter since it gets scaled by var.    
+    %noise is added, Gaussian with mean 0 and std dev = noiseCoef*f(x)    
     data.y =  data.fx+noiseVector; %measurements with noise
     data.Cd = diag(data.y.^2);     % assume sig_f/f = const (constant relative error).
 end
