bc19fd0a6a423c754048aec420b18d2f4da816dd
cmills1095 <56559868+cmills1095@users.noreply.github.com>
2021-02-02 20:19:46 -0800

    One more tweak
    Deleting position statement in the saveFigs

--- saveFigs.m
+++ saveFigs.m
@@ -1,7 +1,5 @@
 function saveFigs(saveFigures,folderName,figNumber)
 if saveFigures
-    set(gcf,'Position',[100 10 1100 600]);
-    %saveas(gcf,[folderName '/Figure ' figNumber],fileType);
     exportgraphics(gcf,[folderName '/Figure ' figNumber '.png']);
     exportgraphics(gcf,[folderName '/Figure ' figNumber '.eps']);
     exportgraphics(gcf,['Allfigure',figNumber,'/',folderName,'.png']);
