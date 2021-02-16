bcd0b4d0fa08f577f3a6405cbd13a30b30b508bf
Max Rudolph <maxrudolph@ucdavis.edu>
2021-02-16 12:47:46 -0800

    workaround for older matlab versions

--- saveFigs.m
+++ saveFigs.m
@@ -1,9 +1,16 @@
 function saveFigs(saveFigures,folderName,figNumber)
 if saveFigures
-    exportgraphics(gcf,[folderName '/Figure ' figNumber '.png']);
-    exportgraphics(gcf,[folderName '/Figure ' figNumber '.eps']);
-    exportgraphics(gcf,['Allfigure',figNumber,'/',folderName,'.png']);
-    exportgraphics(gcf,['Allfigure',figNumber,'/',folderName,'.eps']);
-    close
+    if exist(exportgraphics)
+        exportgraphics(gcf,[folderName '/Figure ' figNumber '.png']);
+        exportgraphics(gcf,[folderName '/Figure ' figNumber '.eps']);
+        exportgraphics(gcf,['Allfigure',figNumber,'/',folderName,'.png']);
+        exportgraphics(gcf,['Allfigure',figNumber,'/',folderName,'.eps']);
+        close
+    else
+        export_fig([folderName '/Figure ' figNumber '.png']);
+        export_fig([folderName '/Figure ' figNumber '.eps']);
+        export_fig(['Allfigure',figNumber,'/',folderName,'.png']);
+        export_fig(['Allfigure',figNumber,'/',folderName,'.eps']);
+    end
 end
 end
