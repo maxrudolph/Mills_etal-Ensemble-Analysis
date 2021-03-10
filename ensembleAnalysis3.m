8742712fbda3f5aafce50b491fd523c99c6c7cd6
Max Rudolph <maxrudolph@ucdavis.edu>
2021-03-09 18:14:58 -0800

    remove merge conflict

--- ensembleAnalysis3.m
+++ ensembleAnalysis3.m
@@ -7,12 +7,9 @@ load(filename,'data','forwardModel','results','measure','pBounds')
 %IF running to generate/save figures, set to true
 % saveFigures = true;
 nxplot=500; %number of measurement points for evaluating ensemble members
-
 nSavedPlot = 2000; %Number of saved runs to plot
+nzplot = 700; %number of imaginary layers to divide models into
 
-nzplot = 1000; %number of imaginary layers to divide models into
-
->>>>>>> 81b73e84b35cfa98e634ca6df209702d249ae076
 meanColor = 'b'; medianColor = 'g';
 
 if saveFigures
@@ -240,7 +237,6 @@ if saveFigures
 end
 disp('Done');
 end
-<<<<<<< HEAD
 
 function [outDepths,outRhos] = shortForm(inDepths,inRhos)
 %removes duplicate values
@@ -251,5 +247,3 @@ outDepths = inDepths;
 outRhos(ind+1) = [];
 outDepths(ind+1) = [];
 end
-=======
->>>>>>> 81b73e84b35cfa98e634ca6df209702d249ae076
