edf55273d11222b90413cabdfe9d9a300b875592
cmills1095 <56559868+cmills1095@users.noreply.github.com>
2021-07-06 21:14:14 -0700

    Getting loop to work in hood

--- BigFigureSavingLoop.m
+++ BigFigureSavingLoop.m
@@ -9,7 +9,7 @@ end
 for i = 1:length(types)
     for j = 1:length(noiseLevels)
 
-        filename = ['~/Ensembles_07022021/' 'Ensemble_',types{i},'_',noiseLevels{j},'_02-Jul-2021.mat']
+        filename = ['/work/cdmills/Senior-Thesis-Stuff/Ensembles_07022021/' 'Ensemble_',types{i},'_',noiseLevels{j},'_02-Jul-2021.mat']
 
         ensembleAnalysisA(filename,true)
         
