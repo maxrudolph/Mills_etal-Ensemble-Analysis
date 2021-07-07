e942bd05deb4f4739532ed5720798b6364baab11
cmills1095 <56559868+cmills1095@users.noreply.github.com>
2021-07-07 11:40:17 -0700

    Update BigFigureSavingLoop.m
    adding parfor to the figure saving loop to run all ensembles at once

--- BigFigureSavingLoop.m
+++ BigFigureSavingLoop.m
@@ -6,7 +6,7 @@ for i = 1:4
     mkdir(['Allfigure',num2str(i)]);
 end
 
-for i = 1:length(types)
+parfor i = 1:length(types)
     for j = 1:length(noiseLevels)
 
         filename = ['/work/cdmills/Senior-Thesis-Stuff/Ensembles_06252021/' 'Ensemble_',types{i},'_',noiseLevels{j},'_25-Jun-2021.mat']
