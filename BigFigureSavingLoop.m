4782c6109f1133caf12082367a47e596b5076e76
cmills1095 <56559868+cmills1095@users.noreply.github.com>
2021-07-26 12:24:35 -0700

    tweaks

--- BigFigureSavingLoop.m
+++ BigFigureSavingLoop.m
@@ -1,5 +1,5 @@
 types = {'1LayerA','3LayerA','4LayerA'};
-noiseLevels = {'0','0.01','0.02','0.05','0.1','0.2'};
+noiseLevels = {'0.01','0.02','0.05','0.1','0.2'};
 addpath(genpath(fileparts(mfilename('fullpath'))))
 
 for i = 1:6
@@ -9,7 +9,7 @@ end
 parfor i = 1:length(types)
     for j = 1:length(noiseLevels)
 
-        filename = ['_',types{i},'_',noiseLevels{j},'_02-Jul-2021.mat']
+        filename = ['_',types{i},'_',noiseLevels{j},'_02-Jul-2021']
 
         figurePlotting(filename,true)
         
