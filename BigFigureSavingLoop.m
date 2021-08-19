91cdf787227002a0021eba66ca22db75adfb7bf1
cmills1095 <56559868+cmills1095@users.noreply.github.com>
2021-08-18 18:37:27 -0700

    Update in prep for analysis re-run

--- BigFigureSavingLoop.m
+++ BigFigureSavingLoop.m
@@ -1,22 +1,21 @@
-types = {'1LayerA','3LayerA','4LayerA'};
+types = {'3LayerA'};
 noiseLevels = {'0','0.01','0.02','0.05','0.1','0.2'};
 addpath(genpath(fileparts(mfilename('fullpath'))))
 
-for i = 1:6
-    mkdir(['Allfigure' num2str(i)]);
-end
 
 parfor i = 1:length(types)
     for j = 1:length(noiseLevels)
 
-        filename = ['_',types{i},'_',noiseLevels{j},'_02-Jul-2021.mat']
+        filename = [...
+            '/work/cdmills/Senior-Thesis-stuff/Ensembles_07022021/Ensemble_',...
+            types{i},'_',noiseLevels{j},'_02-Jul-2021.mat']
 
-        figurePlotting(filename,true)
+        ensembleAnalysisMaster(filename);
         
     end
 end
 
-filename = ['_1LayerA_0_PRIOR_02-Jul-2021.mat'];
-figurePlotting(filename,true);
+%filename = ['_1LayerA_0_PRIOR_02-Jul-2021.mat'];
+%figurePlotting(filename,true);
 
 disp('All done');
