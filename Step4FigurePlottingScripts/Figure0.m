4f449a42bbd976dde857c044a72a05c26cd2cc46
Max Rudolph <maxrudolph@ucdavis.edu>
2021-09-10 17:54:14 -0700

    use same noise pattern for all runs.

--- Step4FigurePlottingScripts/Figure0.m
+++ Step4FigurePlottingScripts/Figure0.m
@@ -1,6 +1,10 @@
 clear;
 close all;
-file_prefix = './Ensembles_07022021/';
+addpath Step3AnalysisScripts
+addpath Step2InversionScripts
+addpath Step4FigurePlottingScripts
+
+file_prefix = './';
 filenames = {
     %'3LayerA_0_02-Jul-2021.mat';
     '3LayerA_0.01_02-Jul-2021.mat';
@@ -22,7 +26,7 @@ load([file_prefix 'Ensemble_' filenames{1}],'results')
 % Assumes the following order of plots:
 % 'Exact solution', 'MS Mean','MS Median','MS Max Likelihood'.'DS Best Fit','DS Median'
 C = [0 0 0;
-   colororder()     
+    colororder()
     ]
 line_widths = {1.5,1.5,1.5,1.5,1.5,1.5};
 line_styles = {'-','-','--','-','--','-'};
@@ -30,45 +34,66 @@ ind = [1,2,2,4,4,2,3,4];
 %displayNames = {'K-Means centroid 1','K-Means centroid 2','K-medians centroid 1','k-medians centroid 2',' ',' ',' '};
 
 h=[];
-for i = 1:numEnsembles    
+for i = 1:numEnsembles
+    figure(figure1);
     load([file_prefix 'Analysis_' filenames{i}]);
     load([file_prefix 'Ensemble_' filenames{i}],'results','data','forwardModel');
     nexttile(i)
     for j=1:length(allModels) % re-assign colors based on indexing into color order above
         allModels{j}.color = C(ind(j),:);
         allModels{j}.lineStyle = line_styles{j};
-   %     if j>1
-  %          allModels{j}.displayName = displayNames{j-1};
-    %    end
+        %     if j>1
+        %          allModels{j}.displayName = displayNames{j-1};
+        %    end
     end
     titles{i}
     importantNumbers = misfitPanel(ewre2n, results,data,forwardModel,[],...
         2*i-1,titles{i},line_widths)
+    % histogram of number of layers
     nexttile(i+numEnsembles)
     histogram(results.ensembleNumLayers,'BinEdges',0.5:10.5,'FaceColor',0.65*[1 1 1]);
     set(gca,'YTick',[]);
     text(0.90,0.95,char(64+2*i),'units','normalized','FontSize',14);
     set(gca,'XTick',1:10);
-    nexttile(i+2*numEnsembles,[2 1])
+    
+    nexttile(i+2*numEnsembles);
+    histogram(results.ensembleVars,'FaceColor',0.65*[1 1 1],'EdgeColor','none');
+    hold on
+    plot(str2num(titles{i})^2*[1 1],get(gca,'YLim'),'r')
+    xlabel('Noise Hyperparameter')
+    
+    nexttile(i+3*numEnsembles,[2 1])
+    % model space pdf
     modelSpacePanel(binCenters,numElements,[],3*i,line_widths);
     set(gca,'ColorScale','linear');
-%     colormap(crameri('lajolla'));
+    %     colormap(crameri('lajolla'));
     colormap(flipud(gray));
-    if i == 1
-        legend('location','southeast')
-        lgd = legend('location','southeast');
-        lgd.FontSize = 7;
-    end
+    % diagnostic plot for misfit
+    figure();
+    n3mask = results.ensembleNumLayers==3;
+    n4mask = results.ensembleNumLayers==4;
+    n5mask = results.ensembleNumLayers==5;
+    histogram(results.ensembleMisfits(n3mask));
+    hold on;
+    histogram(results.ensembleMisfits(n4mask),'EdgeColor','none','FaceColor','r','FaceAlpha',0.5);
+    histogram(results.ensembleMisfits(n5mask),'EdgeColor','none','FaceColor','g','FaceAlpha',0.5);
+    
+    set(gca,'XScale','log')
+    title(titles{i});
+    
+    
 end
-nexttile(1); xticks([.01 .02 .03 .04]);
-nexttile(2); xticks([.05 .1 .2]);
-nexttile(3); xticks(0.1:0.1:0.4);
+% nexttile(1); xticks([.01 .02 .03 .04]);
+% nexttile(2); xticks([.05 .1 .2]);
+% nexttile(3); xticks(0.1:0.1:0.4);
 
 nexttile(1)
 ylabel('Probability');
 nexttile(numEnsembles+1);
+ylabel('Frequency (-)');
+nexttile(3*numEnsembles+1);
 ylabel('Depth (m)');
-nexttile(numEnsembles*2)
+nexttile(numEnsembles*4)
 c=colorbar();
 c.Label.String = 'Probability (normalized)';
 
