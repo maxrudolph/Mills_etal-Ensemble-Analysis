fadff0e88924ba84754dbdd530693cecc34137e2
Max Rudolph <maxwellr@gmail.com>
2023-11-18 11:29:12 -0800

    setup paths

---
+++ setup_paths.m
@@ -0,0 +1,7 @@
+% setup matlab paths for ensemble analysis
+
+addpath Step1.1FieldData/
+addpath Step1DataGenerationScripts/
+addpath Step2InversionScripts/
+addpath Step3AnalysisScripts/
+addpath .
