bc46fb9c0f1670b93c69120a5a5ca14abbe020d6
Max Rudolph <maxwellr@gmail.com>
2024-02-22 13:13:10 -0800

    analysis for Renner

--- main_analysis.m
+++ main_analysis.m
@@ -1,24 +1,10 @@
 clear;
 close all;
 
-% ensemble_files = {
-% 'Ensemble_3LayerA_0.02.mat',
-% 'Ensemble_3LayerA_0.1.mat',
-% 'Ensemble_3LayerA_0.05.mat',
-% 'Ensemble_3LayerA_0.01.mat',
-% 'Ensemble_3LayerA_0.2.mat',
-% 'Ensemble_3LayerA_0.mat'
-% };
-%ensemble_files = {
-%  'Ensemble_Constable1984__hierarchical-1_rhoPrior-2_1_18-Dec-2023.mat',
-%  'Ensemble_Constable1984__hierarchical-1_rhoPrior-1_1_18-Dec-2023.mat'  
-%};
 ensemble_files = {
-  %'Ensemble_3LayerA__hierarchical-1_rhoPrior-1_0.02.mat',
-  %'Ensemble_3LayerA__hierarchical-1_rhoPrior-1_0.05.mat',
-  'Ensemble_3LayerA__hierarchical-1_rhoPrior-1_0.1.mat'
+    'Ensemble_Constable1984_Renner__hierarchical-1_rhoPrior-1_1_22-Feb-2024.mat',
+    'Ensemble_Constable1984_Renner__hierarchical-1_rhoPrior-2_1_22-Feb-2024.mat'  
 };
-
 
 delete(gcp('nocreate'));
 parpool(3);
