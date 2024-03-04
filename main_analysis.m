d7f03bbd8a280730cf9b99ec1dc3c2f7e0939461
Max Rudolph <maxwellr@gmail.com>
2024-03-04 13:43:51 -0800

    postprocess

--- main_analysis.m
+++ main_analysis.m
@@ -7,10 +7,11 @@ ensemble_files = {
     'Ensemble_Constable1984_Wauchope__hierarchical-1_rhoPrior-2_1_04-Mar-2024.mat',
     'Ensemble_Constable1984_Wauchope__hierarchical-1_rhoPrior-2_1_04-Mar-2024'
 };
+exact_known = false;
 
 delete(gcp('nocreate'));
 parpool(3);
 
 for i=1:length(ensemble_files)
-    ensembleAnalysisMaster( ensemble_files{i},false);
+    ensembleAnalysisMaster( ensemble_files{i},exact_known);
 end
