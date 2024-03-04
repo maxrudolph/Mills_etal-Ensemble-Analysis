76cc0a2645ea6bd3686e502daa93f190e84f0a37
Max Rudolph <maxwellr@gmail.com>
2024-03-04 13:42:46 -0800

    postprocess

--- main_analysis.m
+++ main_analysis.m
@@ -1,9 +1,11 @@
 clear;
 close all;
 
-ensemble_files = {
-    'Ensemble_Constable1984_Renner__hierarchical-1_rhoPrior-1_1_22-Feb-2024.mat',
-    'Ensemble_Constable1984_Renner__hierarchical-1_rhoPrior-2_1_22-Feb-2024.mat'  
+ensemble_files = {    
+    % 'Ensemble_Constable1984_Renner__hierarchical-1_rhoPrior-1_1_22-Feb-2024.mat',
+    % 'Ensemble_Constable1984_Renner__hierarchical-1_rhoPrior-2_1_22-Feb-2024.mat'  
+    'Ensemble_Constable1984_Wauchope__hierarchical-1_rhoPrior-2_1_04-Mar-2024.mat',
+    'Ensemble_Constable1984_Wauchope__hierarchical-1_rhoPrior-2_1_04-Mar-2024'
 };
 
 delete(gcp('nocreate'));
