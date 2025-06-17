267f89224939078ef4e014589616c446489f4ddf
Max Rudolph <maxwellr@gmail.com>
2025-06-17 13:47:29 -0700

    add prior sampling

--- main_inversion.m
+++ main_inversion.m
@@ -18,7 +18,7 @@ mex ./Step1DataGenerationScripts/calculateRho1D11_mex.c
 
 %Create data files
 tic
-parfor i = 1:length(noiseLevels)+1
+parfor i = 1:length(noiseLevels)
     if i>length(noiseLevels) %sample the prior for one subStruct
         a = createSyntheticData(0,'subStructChoice','3LayerA');
         b = inversion(a,'priorOn',true);
