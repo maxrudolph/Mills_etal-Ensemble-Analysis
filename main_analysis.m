30557102c8d81c1e39cbf202c9082836450c415f
Max Rudolph <maxrudolph@ucdavis.edu>
2025-08-08 08:21:02 -0700

    update figure scripts

--- main_analysis.m
+++ main_analysis.m
@@ -6,8 +6,9 @@ ensemble_files = {
 %'Ensemble_3LayerA__hierarchical-1_rhoPrior-1_0.05.mat',
 % 'Ensemble_3LayerA__hierarchical-1_rhoPrior-1_0.1.mat',
 %'Ensemble_3LayerA__hierarchical-1_rhoPrior-1_0.mat'
-'Ensemble_Constable1984_Wauchope__hierarchical-1_rhoPrior-2_1_PRIOR',
-'Ensemble_Constable1984_Wauchope__hierarchical-1_rhoPrior-1_1_PRIOR'
+'Ensemble_3LayerA__hierarchical-1_rhoPrior-1_0_PRIOR'
+% 'Ensemble_Constable1984_Wauchope__hierarchical-1_rhoPrior-2_1_PRIOR',
+% 'Ensemble_Constable1984_Wauchope__hierarchical-1_rhoPrior-1_1_PRIOR'
 };
 exact_known = false;
 
